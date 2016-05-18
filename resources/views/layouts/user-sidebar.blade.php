824fec728865f7a1f24eae562021786a7ecd82d2
Dhany Nurdiansyah <dhanyn@outlook.co.id>
2016-05-18 21:06:58 +0700

    tambahan halaman user

---
+++ resources/views/layouts/user-sidebar.blade.php
@@ -0,0 +1,30 @@
+<!-- Sidebar -->
+<div id="sidebar-wrapper">
+    <ul class="sidebar-nav">
+        <li class="sidebar-brand">
+            <a href="#">
+                Start Bootstrap
+            </a>
+        </li>
+        <li>
+            <a href="#">Profile</a>
+        </li>
+        <li>
+            <a href="{{url('usercourse')}}">My Course</a>
+        </li>
+        <li>
+            <a href="#">Achievement</a>
+        </li>
+        <hr/>
+        <li>
+            <a href="#">Home</a>
+        </li>
+        <li>
+            <a href="#">Browse all course</a>
+        </li>
+        <li>
+            <a href="#">Log Out</a>
+        </li>
+    </ul>
+</div>
+<!-- /#sidebar-wrapper -->
--- resources/views/user/index.blade.php
+++ resources/views/user/index.blade.php
@@ -1,70 +1,11 @@
 <html>
     <head>
-        <meta charset="utf-8">
-        <meta http-equiv="X-UA-Compatible" content="IE=edge">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
-
-        <title>@yield('title')</title>
-
-        <script src="{{{ URL::asset('js/jquery.js') }}}"></script>
-        <script src="{{{ URL::asset('js/bootstrap.min.js') }}}"></script>
-
-    <!-- jQuery -->
-        <link href="{{{ asset('css/simple-sidebar.css') }}}" rel="stylesheet"/>
-        <link href="{{{ asset('css/bootstrap.min.css') }}}" rel="stylesheet"/>
-        
-        <!-- Fonts -->
-        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.6.2/css/font-awesome.min.css">
-        <link href="https://fonts.googleapis.com/css?family=Lato:100,300,400,700" rel='stylesheet' type='text/css'>
-        
-        <style>
-            body {
-                font-family: 'Lato';
-            }
-
-            .fa-btn {
-                margin-right: 6px;
-            }
-            .block{
-                display: block;
-            }
-        </style>
+        @include('layouts.user-header')
     </head>
     <body>
         
     <div id="wrapper">
-
-        <!-- Sidebar -->
-        <div id="sidebar-wrapper">
-            <ul class="sidebar-nav">
-                <li class="sidebar-brand">
-                    <a href="#">
-                        Start Bootstrap
-                    </a>
-                </li>
-                <li>
-                    <a href="#">Profile</a>
-                </li>
-                <li>
-                    <a href="#">My Course</a>
-                </li>
-                <li>
-                    <a href="#">Achievement</a>
-                </li>
-                <hr/>
-                <li>
-                    <a href="#">Home</a>
-                </li>
-                <li>
-                    <a href="#">Browse all course</a>
-                </li>
-                <li>
-                    <a href="#">Log Out</a>
-                </li>
-            </ul>
-        </div>
-        <!-- /#sidebar-wrapper -->
-
+        @include('layouts.user-sidebar')
         <!-- Page Content -->
         <div id="page-content-wrapper">
             <div class="container-fluid">
