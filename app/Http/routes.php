987551262a4baae2a04e60f15baaffecd234fb84
fadlifzain <fadlifzain@gmail.com>
2016-06-04 06:31:38 +0700

    routes update

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -13,28 +13,38 @@
 
 
 Route::group(['middleware' => 'web'], function () {
-    Route::auth();
-
-
+    //Login Logout
+    Route::get('/daftar', 'Auth\AuthController@getRegister');
+    Route::get('/masuk', 'Auth\AuthController@getLogin');
+    Route::post('/login', 'Auth\AuthController@login');
     Route::post('/register', 'Auth\AuthController@register');
-
     Route::post('/quick-register', 'Auth\AuthController@quick_register');
-
+    
     Route::get('/', 'HomeController@beranda');
+    Route::get('/beranda', 'HomeController@beranda');
     Route::get('/kursus', 'HomeController@kursus');
-    Route::get('/kami', 'HomeController@kami');
+    Route::get('/kursus/{course_id}', 'HomeController@kursus-detil');
+    Route::get('/tentangkami', 'HomeController@tentangkami');
 
     Route::group(['middleware' => 'auth'], function()
     {
-      Route::get('/dashboard', function() {
-        return view('user.index');
-      });
+      Route::get('/dashboard', 'DashboardController@dashboard';
+
+      Route::get('/dashboard/profil', 'DashboardController@profil');
+
+      Route::get('/dashboard/kursusku', 'DashboardController@kursusku');
+
+      Route::get('/dashboard/kursusku/{course_id}', 'DashboardController@kursusku-detil');
+
+      Route::get('/dashboard/kursusku/{course_id}/{lesson_title}', 'DashboardController@belajar');
+
+      Route::get('/dashboard/penghargaan', 'DashboardController@penghargaan');
+
+      Route::get('/keluar', 'Auth\AuthController@logout');
     });
 
     Route::group(['middleware' => 'admin'], function()
     {
-      Route::get('/admin', function() {
-        return views('administrator.index');
-      });
+      Route::get('/admin-dashboard/', 'AdminController@dashboard');
     });
 });
